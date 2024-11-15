8e75e106021700442e4dafbb543a86368c3d62ae
Martin Schneiter <martin.schneiter@gmx.ch>
2024-11-15 23:39:05 +0100

    Improve/simplify code

--- src/crosscorrelation_zp.py
+++ src/crosscorrelation_zp.py
@@ -14,47 +14,33 @@ def ccorr2D_zp(signal1, signal2, mask=None, normalize=True, centering=True):
     input signals
     """
 
-    # the computation is more safe and simple when we make first sure 
-    # that the signals have an even number of elements along both dimensions 
-    # along the width and the height
     ni, nj = signal1.shape
-
-    #print('*****************************************************************')
-    #print('ni1: ',ni,'nj1: ',nj)
-    #print('*****************************************************************')
 
     # if mask is None (default), we assume that all pixels in signal are valid: 
     if mask is None:
         mask = numpy.ones_like(signal1)
 
-    # make sure there are no nan-values:
+    # make sure there are no NaN-values:
     for i in range(ni):
         for j in range(nj):
             if (not mask[i,j]):
                 signal1[i,j] = 0.0
                 signal2[i,j] = 0.0
 
-    if (ni % 2):
-        # if ni mod 2 == 1 then ni is an odd number -> remove one row
-        signal1 = signal1[0:ni-1,:]
-        signal2 = signal2[0:ni-1,:]
-        mask = mask[0:ni-1,:]
-
-    if (nj % 2):
-        signal1 = signal1[:,0:nj-1]
-        signal2 = signal2[:,0:nj-1]
-        mask = mask[:,0:nj-1]
-
     # the signal (and the mask) has now an even number of rows and columns 
     ni, nj = signal1.shape
-
-    #print('*****************************************************************')
-    #print('ni: ',ni,'nj: ',nj)
-    #print('*****************************************************************')
 
     # Get the signals' mean (excluding missing values)
     mean1 = numpy.sum(numpy.multiply(signal1, mask)) / numpy.sum(mask)
     mean2 = numpy.sum(numpy.multiply(signal2, mask)) / numpy.sum(mask)
+
+    # Missing numbers are now set to the signal's mean
+    # This way, missing numbers will then be zero after centering
+    for i in range(ni):
+        for j in range(nj):
+            if (not mask[i,j]):
+                signal1[i,j] = mean1
+                signal2[i,j] = mean2
 
     # Get a centered version of the signals
     if (centering):
@@ -69,9 +55,9 @@ def ccorr2D_zp(signal1, signal2, mask=None, normalize=True, centering=True):
     padded_signal1 = numpy.zeros((2*ni, 2*nj))
     padded_signal2 = numpy.zeros((2*ni, 2*nj))
 
-    # fill in the signal:
-    padded_signal1[int(ni/2):int(3*ni/2),int(nj/2):int(3*nj/2)] = centered_signal1
-    padded_signal2[int(ni/2):int(3*ni/2),int(nj/2):int(3*nj/2)] = centered_signal2
+    # Fill in the signal:
+    padded_signal1[0:ni, 0:nj] = centered_signal1
+    padded_signal2[0:ni, 0:nj] = centered_signal2
 
     # The Fourier transform is computed using the FFT
     fft_signal1 = numpy.fft.fft2(padded_signal1)
@@ -81,34 +67,33 @@ def ccorr2D_zp(signal1, signal2, mask=None, normalize=True, centering=True):
     # of the power spectral density 
     # (due to missing values substitued by zero and the zero-padding)
 
-    pseudo_powerSpectralDensity = numpy.multiply(fft_signal1,
-        numpy.conjugate(fft_signal2))
+    pseudo_powerSpectralDensity = numpy.abs(numpy.multiply(fft_signal1,
+        numpy.conjugate(fft_signal2)))
     pseudo_crosscovariance = numpy.real( numpy.fft.ifft2(
         pseudo_powerSpectralDensity))
 
     # We repeat the same process (except for centering) on a masked_signal
     # in order to estimate the error made on the previous computation 
-    # the mask_signal has as entries ones (for signal) and zeros (for no signal) 
-
+    # the mask_signal has as entries ones (for signal) and zeros (for no signal)
     masked_signal = numpy.zeros((2*ni, 2*nj))
-    masked_signal[int(ni/2):int(3*ni/2),int(nj/2):int(3*nj/2)] = mask
+    masked_signal[0:ni,0:nj] = mask
 
     fft_masked_signal = numpy.fft.fft2(masked_signal)
-    mask_correction_factors = numpy.real(numpy.fft.ifft2( numpy.multiply(
+    mask_correction_factors = numpy.abs(numpy.fft.ifft2( numpy.multiply(
         fft_masked_signal, numpy.conjugate(fft_masked_signal))))
 
     # The "error" made can now be easily corrected by an element-wise division
     crosscovariance = pseudo_crosscovariance / mask_correction_factors
 
-    #variance = crosscovariance.flat[0]
+
 
     # fft-shift:
     crosscovariance = numpy.fft.fftshift(crosscovariance)
     crosscovariance = crosscovariance[int(ni/2):int(3*ni/2),int(nj/2):int(3*nj/2)]
 
     if (normalize):
-        var1 = numpy.var(centered_signal1)
-        var2 = numpy.var(centered_signal2)
+        var1 = numpy.sum(numpy.multiply(centered_signal1,centered_signal1)) / numpy.sum(mask)
+        var2 = numpy.sum(numpy.multiply(centered_signal2,centered_signal2)) / numpy.sum(mask)
     else:
         var1 = 1.0
         var2 = 1.0
